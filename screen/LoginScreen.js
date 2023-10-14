9628ffc11e09d69d6708290226fae5537e4e3360
MuhammadZubair786 <mzhassan444@gmail.com>
2023-10-14 04:33:57 -0700

    firebase add

--- screen/LoginScreen.js
+++ screen/LoginScreen.js
@@ -1,18 +1,84 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { Alert, View } from 'react-native';
 import { TextInput, Text, RadioButton, Button } from 'react-native-paper';
+import auth from '@react-native-firebase/auth'
+import Snackbar from 'react-native-snackbar';
+
 const MyComponent = () => {
+
+    const [email,setEmail]=useState("")
+    const [password,setPassword]=useState("")
+
+
     const [showpassword, setshowpassword] = useState(true)
     const [checked, setChecked] = useState('first');
+
+    const createEmailPassword=()=>{
+        if(email=="" ){
+            Snackbar.show({
+                text: 'Plz Enter Email Address',
+                duration: Snackbar.LENGTH_SHORT,
+                backgroundColor:"grey",
+                textColor:"red"
+              });
+        }
+        else if(password=="" ){
+            Snackbar.show({
+                text: 'Plz Enter Password',
+                duration: Snackbar.LENGTH_SHORT,
+                margin:20,
+                backgroundColor:"grey",
+                textColor:"white"
+              });
+        }
+        else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)==false){
+            Snackbar.show({
+                text: 'Plz Enter Valid Email',
+                duration: Snackbar.LENGTH_SHORT,
+                backgroundColor:"grey",
+                textColor:"red"
+              });
+
+        }
+        else if(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(password)==false){
+            Snackbar.show({
+                text: 'Plz valid password',
+                duration: Snackbar.LENGTH_SHORT,
+                backgroundColor:"grey",
+                textColor:"red"
+              });
+        }
+
+        else{
+            console.log(email)
+            auth().createUserWithEmailAndPassword(email,password)
+            .then((res)=>{
+                console.log(res.user.uid)
+            })
+            .catch((err)=>{
+               if(err.code=="auth/email-already-in-use"){
+                Alert.alert("Email ALready regsiter",
+                )
+               }
+            })
+        }
+
+    }
+
     return (
         <View>
             <TextInput
+
+            onChangeText={(e)=>setEmail(e)}
+            value={email}
                 mode="outlined"
                 style={{ backgroundColor: "white", color: "blue", margin: 20 }}
                 label="Email"
                 left={<TextInput.Icon icon="email" color={"red"} />}
             />
             <TextInput
+            value={password}
+            onChangeText={(e)=>setPassword(e)}
                 mode="outlined"
                 style={{ backgroundColor: "white", color: "blue", marginVertical: 2, marginHorizontal: 20 }}
                 label="Password"
@@ -32,10 +98,10 @@ const MyComponent = () => {
                 <RadioButton.Item label="FEMALE" value="second" />
             </RadioButton.Group>
 
-            <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+            <Button icon="camera" mode="contained" onPress={() => createEmailPassword()}>
                 Press me
             </Button>
-            <Snackbar
+            {/* <Snackbar
         visible={visible}
         onDismiss={onDismissSnackBar}
         action={{
@@ -45,7 +111,7 @@ const MyComponent = () => {
           },
         }}>
         Hey there! I'm a Snackbar.
-      </Snackbar>
+      </Snackbar> */}
         </View>
 
 
